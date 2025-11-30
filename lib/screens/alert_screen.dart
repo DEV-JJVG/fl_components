ffb6db7bd9a88f13885031ea3e4804da61906162
DEV-JJVG <204841338+DEV-JJVG@users.noreply.github.com>
2025-11-30 14:18:11 +0100

    Agregamos la validación de los campos introducidos en el formulario y si no se introduce uno correcto que ponga uno por defecto.

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -2,9 +2,58 @@ import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
+  void displayDialog(BuildContext context) {
+    print('Hola buenas tardes como estan');
+    showDialog(
+      //para que nos muestre un mensaje cuando le demos al boton
+      barrierDismissible: false, //para que solo pueda salir dandole a un boton
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(
+            'Alerta no hay nada aqui; solo estas perdiendo el tiempo',
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                'Aqui es donde va el contenido de la alerta; la cual; no hay nada',
+              ),
+              SizedBox(height: 20),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            //para que el usuario pueda cerrar la ventana
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cierre esta ventana que no sirve para nada'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Alert Screen")));
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          //sale un boton en el cual se mostrara una alerta
+          onPressed: () {
+            displayDialog(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Se mostro una alerta', style: TextStyle(fontSize: 20)),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.wifi_calling_3_sharp),
+        onPressed: () => Navigator.pop(context),
+      ),
+    );
   }
 }
